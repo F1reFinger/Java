a6420f10f0e4d9fbb3059a077837bf6e2fe3bd1d
F1reFinger <180121847@aluno.unb.br>
2022-02-16 01:56:45 -0300

    java files modified and updated

--- src/com/MyFirstProgramm.java
+++ src/com/MyFirstProgramm.java
@@ -6,7 +6,7 @@ public class  MyFirstProgramm{
 
 
     public static void main(String[] args){
-        Orider order = new Orider("Code1234");
+        Orider order = new Orider("Code1234", 99.99);
         System.out.println(order);
     }
 }
--- src/com/base/Orider.java
+++ src/com/base/Orider.java
@@ -1,15 +1,48 @@
 package com.base;
 
+import java.math.BigDecimal;
+
+/*
+* @author Helder Lourenço ded Abreu Marques
+* @version 1.0.0
+* @see BigDecimal
+* @since 1.0.0
+*/
+
 public class Orider{
 
     private final String code;
+    private final BigDecimal totalValue;
 
-    public Orider(String code){
-        this.code = code;
+    /**
+     * construtor da classe 
+     *  @param code         codigo do pedido
+     *  @param totalValue   valor total do pedido
+     */
+
+    public Orider(String code, double totalValue){
+        this.code = code;  
+        this.totalValue = totalValue;
     }
 
     @Override
     public String toString(){
         return "Order={" + "code='" + code + "'" + "}";
+    }
+
+    /**
+     * calcula o valor total do pedido se o valor maior que 100 uma taxa será cobrada
+     * 
+     * @return  retorna o valor do pedido com as taxas 
+     * @throws RuntimeException lança excessão caso o valor seja menor que 100  
+     */
+    public double calculateFee (){
+        if(this.totalValue.signum() < 0){
+            throw new RuntimeException("O pedido não pode ter valor negativo");
+        }
+        if(this.totalValuecompareTo(new BigDecimal("00.00") > 100 )){
+            return this.totalValue.multiply(new BigDecimal("00.00"));
+        }
+        return this.totalValue;
     }
 }
